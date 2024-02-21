dbf1f4f42a643623b6014f5993fb31587946ac91
sauce <dadad1004@naver.com>
2024-02-21 23:37:59 +0900

    Auth working on

--- src/schema/env.schema.ts
+++ src/schema/env.schema.ts
@@ -10,6 +10,9 @@ declare module 'fastify' {
 const schema = Type.Object({
   // Add your environment variables here
   PORT: Type.Integer({ minimum: 0, maximum: 65535, default: 3000 }),
+  DATABASE_URL: Type.String({ format: "uri" }),
+  REDIS_HOST: Type.String({ format: "hostname", default: "localhost" }),
+  REDIS_PORT: Type.Integer({ minimum: 0, maximum: 65535, default: 6379 }),
 })
 
 type T = Static<typeof schema>
