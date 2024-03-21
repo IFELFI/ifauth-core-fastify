1605097a5da73e17f3fcf61ac1300bfe152e6b53
sauce-git <sauce@mandacode.com>
2024-03-21 21:40:22 +0900

    Setup and env schema changed

--- src/schema/env.schema.ts
+++ src/schema/env.schema.ts
@@ -1,4 +1,5 @@
 import { Static, Type } from "@sinclair/typebox";
+import bcrypt from 'bcrypt'
 import 'fastify'
 
 declare module 'fastify' {
@@ -13,10 +14,11 @@ const schema = Type.Object({
   PORT: Type.Integer({ minimum: 0, maximum: 65535, default: 3000 }),
   TOKEN_SECRET: Type.String(),
   COOKIE_SECRET: Type.String(),
-  ACCESS_TOKEN_EXPIRATION: Type.String({ default: "5m" }), 
+  ACCESS_TOKEN_EXPIRATION: Type.String({ default: "5m" }),
   REFRESH_TOKEN_EXPIRATION: Type.String({ default: "1d" }),
   DATABASE_URL: Type.String(),
-  REDIS_URL: Type.String({default: "redis://localhost:6379"}),
+  REDIS_URL: Type.String({ default: "redis://localhost:6379" }),
+  SALT: Type.String({ default: bcrypt.genSaltSync(10) }),
 })
 
 type T = Static<typeof schema>
