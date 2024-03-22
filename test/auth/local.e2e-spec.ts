a6717b10be5880a7785473629e5b7c70a516678d
sauce-git <sauce@mandacode.com>
2024-03-22 15:36:22 +0900

    e2e tests updated

--- test/auth/local.e2e-spec.ts
+++ test/auth/local.e2e-spec.ts
@@ -17,40 +17,136 @@ describe("Auth local", () => {
 
   afterAll(async () => {
     await server.close();
-  })
+  });
 
-  describe("[POST] /auth/local", () => {
-    describe("Signup", () => {
-      it("should return 200 and a token when signup with proper data", async () => {
-        const response = await server.inject({
-          method: 'POST',
-          url: '/auth/local/signup',
-          payload: {
-            email: 'test@ifelfi.com',
-            password: 'password'
-          }
-        });
-        expect(response.statusCode).toBe(201);
-        expect(response.headers['authorization']).toBeDefined();
-        expect(response.cookies).toBeDefined();
+  describe("[POST] /auth/local/signup", () => {
+    it("should return 200 and a token when signup with proper data", async () => {
+      const response = await server.inject({
+        method: 'POST',
+        url: '/auth/local/signup',
+        payload: {
+          email: 'test@ifelfi.com',
+          password: 'password'
+        }
       });
+      expect(response.statusCode).toBe(201);
+      expect(response.headers['authorization']).toBeDefined();
+      expect(response.cookies).toBeDefined();
     });
 
-    describe("Login", () => {
-      it("should return 200 and a token when login with proper data", async () => {
-        await createDefaultLocalUser();
-        const response = await server.inject({
-          method: 'POST',
-          url: '/auth/local/login',
-          payload: {
-            email: 'test@ifelfi.com',
-            password: 'password'
-          }
-        });
-        expect(response.statusCode).toBe(200);
-        expect(response.headers['authorization']).toBeDefined();
-        expect(response.cookies).toBeDefined();
+    it("should return 400 when signup with invalid data", async () => {
+      const response = await server.inject({
+        method: 'POST',
+        url: '/auth/local/signup',
+        payload: {
+          email: 'ifelfi.com',
+        }
       });
+      expect(response.statusCode).toBe(400);
+    });
+
+    it("should return 400 when signup with an existing email", async () => {
+      await createDefaultLocalUser();
+      const response = await server.inject({
+        method: 'POST',
+        url: '/auth/local/signup',
+        payload: {
+          email: 'test@ifelfi.com',
+          password: 'password'
+        }
+      });
+      expect(response.statusCode).toBe(409);
+    });
+
+    it('should return 400 when signup with an invalid email', async () => {
+      const response = await server.inject({
+        method: 'POST',
+        url: '/auth/local/signup',
+        payload: {
+          email: 'test',
+          password: 'password'
+        }
+      });
+      expect(response.statusCode).toBe(400);
+    })
+
+    it('should return 400 when signup with an invalid short password', async () => {
+      const response = await server.inject({
+        method: 'POST',
+        url: '/auth/local/signup',
+        payload: {
+          email: 'test@ifelfi.com',
+          password: 'pass'
+        }
+      });
+      expect(response.statusCode).toBe(400);
+    });
+
+    it('should return 400 when signup with an invalid long password', async () => {
+      const response = await server.inject({
+        method: 'POST',
+        url: '/auth/local/signup',
+        payload: {
+          email: 'test@ifelfi.com',
+          password: String('a').repeat(257)
+        }
+      });
+      expect(response.statusCode).toBe(400);
+    });
+  });
+
+  describe("[POST] /auth/local/login", () => {
+    it("should return 200 and a token when login with proper data", async () => {
+      await createDefaultLocalUser();
+      const response = await server.inject({
+        method: 'POST',
+        url: '/auth/local/login',
+        payload: {
+          email: 'test@ifelfi.com',
+          password: 'password'
+        }
+      });
+      expect(response.statusCode).toBe(200);
+      expect(response.headers['authorization']).toBeDefined();
+      expect(response.cookies).toBeDefined();
+    });
+
+    it("should return 400 when login with invalid data", async () => {
+      await createDefaultLocalUser();
+      const response = await server.inject({
+        method: 'POST',
+        url: '/auth/local/login',
+        payload: {
+          email: 'test@ifelfi.com'
+        }
+      });
+      expect(response.statusCode).toBe(400);
+    });
+
+    it("should return 401 when login with invalid password", async () => {
+      await createDefaultLocalUser();
+      const response = await server.inject({
+        method: 'POST',
+        url: '/auth/local/login',
+        payload: {
+          email: 'test@ifelfi.com',
+          password: 'wrong_password'
+        }
+      });
+      expect(response.statusCode).toBe(401);
+    });
+
+    it("should return 401 when login with invalid email", async () => {
+      await createDefaultLocalUser();
+      const response = await server.inject({
+        method: 'POST',
+        url: '/auth/local/login',
+        payload: {
+          email: 'wrong@ifelfi.com',
+          password: 'password'
+        }
+      });
+      expect(response.statusCode).toBe(401);
     });
   });
 });
---
+++ test/user.e2e-spec.ts
@@ -0,0 +1,86 @@
+import { it, afterAll, beforeAll, describe, expect, beforeEach } from "@jest/globals";
+import { FastifyInstance } from "fastify";
+import build from "../src/app";
+import { accessToken, createDefaultLocalUser, expiredAccessToken, postgresContainer, redisClient, redisContainer, refreshToken, signer } from "./setup-e2e";
+
+describe("User", () => {
+  let server: FastifyInstance;
+  let createdUser: { uuidKey: string, accessToken: string, expiredAccessToken: string };
+
+  beforeAll(async () => {
+    server = await build({}, {
+      ...process.env,
+      DATABASE_URL: postgresContainer.getConnectionUri(),
+      REDIS_URL: redisContainer.getConnectionUrl(),
+    });
+  })
+
+  afterAll(async () => {
+    await server.close();
+  });
+
+  beforeEach(async () => {
+    const createdUser = await createDefaultLocalUser();
+    await redisClient.set(createdUser.uuidKey, refreshToken);
+  })
+
+  describe("[GET] /user/logout", () => {
+    it("should return 200 when logout", async () => {
+      const response = await server.inject({
+        method: 'GET',
+        url: '/user/logout',
+        headers: {
+          'Authorization': `Bearer ${createdUser.accessToken}`
+        },
+        cookies: {
+          'refresh': signer.sign(refreshToken)
+        }
+      });
+      expect(response.statusCode).toBe(200);
+    });
+
+    it("should return 200 when logout without access token but refresh token is valid", async () => {
+      const response = await server.inject({
+        method: 'GET',
+        url: '/user/logout',
+        headers: {
+          'Authorization': `Bearer ${createdUser.expiredAccessToken}`
+        }
+      });
+      expect(response.statusCode).toBe(401);
+    });
+
+    it("should return 401 when logout without access and refresh token", async () => {
+      const response = await server.inject({
+        method: 'GET',
+        url: '/user/logout',
+      });
+      expect(response.statusCode).toBe(401);
+    });
+
+    it("should return 404 when logout with an invalid access token", async () => {
+      const response = await server.inject({
+        method: 'GET',
+        url: '/user/logout',
+        headers: {
+          'Authorization': `Bearer ${accessToken}`
+        }
+      });
+      expect(response.statusCode).toBe(404);
+    });
+
+    it("should return 401 when logout with an expired access token", async () => {
+      const response = await server.inject({
+        method: 'GET',
+        url: '/user/logout',
+        headers: {
+          'Authorization': `Bearer ${createdUser.expiredAccessToken}`
+        },
+        cookies: {
+          'refresh': signer.sign(refreshToken)
+        }
+      });
+      expect(response.statusCode).toBe(401);
+    });
+  });
+})
