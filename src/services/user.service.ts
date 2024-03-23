bc5956b223858b60f94020468c54e406093e8ad4
sauce-git <sauce@mandacode.com>
2024-03-23 11:52:45 +0900

    Delete user added

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance } from 'fastify';
 
 export class UserService {
   #fastify: FastifyInstance;
@@ -13,16 +13,31 @@ export class UserService {
    * @returns Promise of logout result
    */
   public async logout(uuidKey: string): Promise<boolean> {
-    const findUser = await this.#fastify.prisma.users.findUnique({ where: { uuid_key: uuidKey } });
+    const findUser = await this.#fastify.prisma.users.findUnique({
+      where: { uuid_key: uuidKey },
+    });
     if (!findUser) {
-      throw this.#fastify.httpErrors.notFound("User not found");
+      throw this.#fastify.httpErrors.notFound('User not found');
     }
     // Delete refresh token from Redis
     await this.#fastify.redis.del(uuidKey);
     return true;
   }
 
-  public async deleteUser() {
-
+  /**
+   * Delete user
+   * @param uuidKey User's UUID key
+   * @returns Promise of delete user result
+   */
+  public async deleteUser(uuidKey: string): Promise<boolean> {
+    await this.#fastify.prisma.users
+      .delete({ where: { uuid_key: uuidKey } })
+      .catch((error) => {
+        throw this.#fastify.httpErrors.internalServerError(error.message);
+      });
+    await this.#fastify.redis.del(uuidKey).catch((error) => {
+      throw this.#fastify.httpErrors.internalServerError(error.message);
+    });
+    return true;
   }
 }
