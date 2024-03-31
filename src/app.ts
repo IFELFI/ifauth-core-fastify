fffa31f395209e6453d5de9d6a1ca18363aa1976
sauce-Git <dadad1004@naver.com>
2024-03-31 23:37:54 +0900

    Add exposedHeaders option to CORS configuration

--- src/app.ts
+++ src/app.ts
@@ -96,6 +96,7 @@ async function build(opts: {}, data: any = process.env) {
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Authorization'],
   });
 
   // Register routes
