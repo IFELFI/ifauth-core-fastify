e19a34c5f1d9e9f864b4d9d91356a04e8f97fa2c
sauce-Git <dadad1004@naver.com>
2024-04-21 15:55:12 +0900

    Update unit tests

--- src/services/auth/local.service.spec.ts
+++ src/services/auth/local.service.spec.ts
@@ -26,6 +26,24 @@ describe('AuthLocalService', () => {
     fastify = mockDeep<FastifyInstance>();
     service = new AuthLocalService(fastify);
 
+    // mock config
+    fastify.config = {
+      HOST: 'localhost',
+      PORT: 3000,
+      ACCESS_TOKEN_EXPIRATION: 60 * 5,
+      REFRESH_TOKEN_EXPIRATION: 60 * 60 * 24 * 3,
+      DATABASE_URL: 'postgres://localhost:5432',
+      REDIS_URL: 'redis://localhost:6379',
+      TOKEN_SECRET: 'secret',
+      COOKIE_SECRET: 'secret',
+      SALT: 'salt',
+      AUTH_CODE_EXPIRATION: 60 * 3,
+    };
+
+    // mock redis methods
+    jest.spyOn(fastify.redis, 'set').mockResolvedValue('OK');
+
+    // mock fastify error methods
     jest
       .spyOn(fastify.httpErrors, 'conflict')
       .mockImplementation((msg?: string) => {
@@ -51,20 +69,6 @@ describe('AuthLocalService', () => {
       .mockImplementation((payload, options) =>
         jwt.sign(payload, secret, { expiresIn: options.expiresIn }),
       );
-    jest.spyOn(fastify.redis, 'set').mockResolvedValue('OK');
-
-    fastify.config = {
-      HOST: 'localhost',
-      PORT: 3000,
-      ACCESS_TOKEN_EXPIRATION: 60 * 5,
-      REFRESH_TOKEN_EXPIRATION: 60 * 60 * 24 * 3,
-      DATABASE_URL: 'postgres://localhost:5432',
-      REDIS_URL: 'redis://localhost:6379',
-      TOKEN_SECRET: 'secret',
-      COOKIE_SECRET: 'secret',
-      SALT: 'salt',
-      AUTH_CODE_EXPIRATION: 60 * 3,
-    };
   });
 
   describe('signup', () => {
--- src/services/user.service.spec.ts
+++ src/services/user.service.spec.ts
@@ -3,61 +3,69 @@ import { UserService } from './user.service';
 import { FastifyInstance } from 'fastify';
 import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
 import { v4 as uuidv4 } from 'uuid';
+import { users, profile } from '@prisma/client';
 
 describe('UserService', () => {
-  const uuidKey = uuidv4();
-  const email = 'test@ifelfi.com';
   let service: UserService;
   let fastify: DeepMockProxy<FastifyInstance>;
+
+  // setup basic data for testing
+  const user: users = {
+    id: 1,
+    email: 'test@ifelfi.com',
+    uuid_key: uuidv4(),
+  };
+  const profile: profile = {
+    id: 1,
+    user_id: user.id,
+    nickname: 'test',
+    image_url: null,
+    join_date: new Date(),
+    update_date: new Date(),
+  };
+
   beforeAll(() => {
     fastify = mockDeep<FastifyInstance>();
     service = new UserService(fastify);
 
+    // mock fastify error methods
     jest
       .spyOn(fastify.httpErrors, 'internalServerError')
       .mockImplementation((msg?: string) => {
         throw new Error(msg || 'Internal server error');
       });
-
     jest
       .spyOn(fastify.httpErrors, 'notFound')
       .mockImplementation((msg?: string) => {
         throw new Error(msg || 'Not found');
       });
   });
+
   describe('logout', () => {
     it('should logout user', async () => {
-      jest.spyOn(fastify.prisma.users, 'findUnique').mockResolvedValue({
-        id: 1,
-        uuid_key: uuidKey,
-        email,
-      });
+      jest.spyOn(fastify.prisma.users, 'findUnique').mockResolvedValue(user);
       jest.spyOn(fastify.redis, 'del').mockResolvedValue(1);
-      expect(await service.logout(uuidKey)).toBe(true);
+      expect(await service.logout(user.uuid_key)).toBe(true);
     });
 
     it('should throw not found error', async () => {
       jest.spyOn(fastify.prisma.users, 'findUnique').mockResolvedValue(null);
-      await expect(service.logout(uuidKey)).rejects.toThrow('User not found');
+      await expect(service.logout(user.uuid_key)).rejects.toThrow('User not found');
     });
 
     it('should throw internal server error', async () => {
       jest
         .spyOn(fastify.prisma.users, 'findUnique')
         .mockRejectedValue(new Error());
-      await expect(service.logout(uuidKey)).rejects.toThrow(
+      await expect(service.logout(user.uuid_key)).rejects.toThrow(
         'Failed to find user',
       );
     });
 
     it('should throw internal server error when delete refresh token from server', async () => {
-      jest.spyOn(fastify.prisma.users, 'findUnique').mockResolvedValue({
-        id: 1,
-        uuid_key: uuidKey,
-        email,
-      });
+      jest.spyOn(fastify.prisma.users, 'findUnique').mockResolvedValue(user);
       jest.spyOn(fastify.redis, 'del').mockRejectedValue(new Error());
-      await expect(service.logout(uuidKey)).rejects.toThrow(
+      await expect(service.logout(user.uuid_key)).rejects.toThrow(
         'Failed to delete refresh token from server',
       );
     });
@@ -65,30 +73,22 @@ describe('UserService', () => {
 
   describe('deleteUser', () => {
     it('should delete user', async () => {
-      jest.spyOn(fastify.prisma.users, 'delete').mockResolvedValue({
-        id: 1,
-        uuid_key: uuidKey,
-        email,
-      });
+      jest.spyOn(fastify.prisma.users, 'delete').mockResolvedValue(user);
       jest.spyOn(fastify.redis, 'del').mockResolvedValue(1);
-      expect(await service.deleteUser(uuidKey)).toBe(true);
+      expect(await service.deleteUser(user.uuid_key)).toBe(true);
     });
 
     it('should throw internal server error', async () => {
       jest.spyOn(fastify.prisma.users, 'delete').mockRejectedValue(new Error());
-      await expect(service.deleteUser(uuidKey)).rejects.toThrow(
+      await expect(service.deleteUser(user.uuid_key)).rejects.toThrow(
         'Failed to delete user',
       );
     });
 
     it('should throw internal server error when delete refresh token from server', async () => {
-      jest.spyOn(fastify.prisma.users, 'delete').mockResolvedValue({
-        id: 1,
-        uuid_key: uuidKey,
-        email,
-      });
+      jest.spyOn(fastify.prisma.users, 'delete').mockResolvedValue(user);
       jest.spyOn(fastify.redis, 'del').mockRejectedValue(new Error());
-      await expect(service.deleteUser(uuidKey)).rejects.toThrow(
+      await expect(service.deleteUser(user.uuid_key)).rejects.toThrow(
         'Failed to delete refresh token from server',
       );
     });
