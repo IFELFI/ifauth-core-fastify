df70755866de085deadfd7361f99edce6d7e71c5
sauce-git <sauce@mandacode.com>
2024-04-21 15:55:12 +0900

    Update unit tests

--- src/services/token.service.spec.ts
+++ src/services/token.service.spec.ts
@@ -1,26 +1,44 @@
 import { it, beforeAll, describe, expect, jest } from '@jest/globals';
 import { v4 as uuidv4 } from 'uuid';
-import jwt from 'jsonwebtoken';
+import jwt, { JsonWebTokenError } from 'jsonwebtoken';
 import { AccessTokenPayload } from '../interfaces/token.interface';
 import { FastifyInstance } from 'fastify';
 import { TokenService } from './token.service';
 import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
 import { profile, users } from '@prisma/client';
 import { randomBytes } from 'crypto';
+import { httpErrors } from '@fastify/sensible';
 
 describe('TokenService', () => {
   let service: TokenService;
   let fastify: DeepMockProxy<FastifyInstance>;
-  const uuidKey = uuidv4();
-  const code = randomBytes(16).toString('hex');
 
-  const accessPayload: AccessTokenPayload = {
-    uuidKey,
+  // setup basic data for testing
+  const user: users = {
+    id: 1,
     email: 'test@ifelfi.com',
-    nickname: 'test',
-    imageUrl: null,
+    uuid_key: uuidv4(),
   };
+  const profile: profile = {
+    id: 1,
+    user_id: user.id,
+    nickname: 'test',
+    image_url: null,
+    join_date: new Date(),
+    update_date: new Date(),
+  };
+
+  // payload data
+  const accessPayload: AccessTokenPayload = {
+    uuidKey: user.uuid_key,
+    email: user.email,
+    nickname: profile.nickname,
+    imageUrl: profile.image_url,
+  };
+
+  // token data
   const secret = 'secret';
+  const code = randomBytes(16).toString('hex');
   const accessToken = jwt.sign(accessPayload, secret, { expiresIn: '2h' });
   const refreshToken = jwt.sign({}, secret, { expiresIn: '2d' });
   const expiredAccessToken = jwt.sign(accessPayload, secret, {
@@ -37,17 +55,6 @@ describe('TokenService', () => {
     fastify = mockDeep<FastifyInstance>();
     service = new TokenService(fastify);
 
-    jest.spyOn(fastify.jwt, 'verify').mockImplementation((token) => {
-      return jwt.verify(token, secret);
-    });
-    jest.spyOn(fastify.jwt, 'decode').mockImplementation((token) => {
-      return jwt.decode(token);
-    });
-    jest.spyOn(fastify.jwt, 'sign').mockImplementation((payload, options) => {
-      const token = jwt.sign(payload, secret, { expiresIn: options.expiresIn });
-      return token;
-    });
-
     // mock config
     fastify.config = {
       HOST: 'localhost',
@@ -62,39 +69,43 @@ describe('TokenService', () => {
       AUTH_CODE_EXPIRATION: 60 * 3,
     };
 
-    jest.spyOn(fastify.redis, 'set').mockResolvedValue('OK');
-    jest.spyOn(fastify.redis, 'get').mockImplementation((key) => {
-      return new Promise((resolve) => {
-        if (key === uuidKey) {
-          resolve(refreshToken);
-        } else if (key === code) {
-          resolve('1');
-        } else {
-          resolve(null);
-        }
-      });
-    });
-    jest.spyOn(fastify.redis, 'del').mockResolvedValue(1);
+    /**
+     * Mock Fastify instance methods
+     */
 
+    // mock jwt methods
+    jest.spyOn(fastify.jwt, 'verify').mockImplementation((token) => {
+      return jwt.verify(token, secret);
+    });
+    jest.spyOn(fastify.jwt, 'decode').mockImplementation((token) => {
+      return jwt.decode(token);
+    });
+    jest.spyOn(fastify.jwt, 'sign').mockImplementation((payload, options) => {
+      const token = jwt.sign(payload, secret, { expiresIn: options.expiresIn });
+      return token;
+    });
+
+    // mock fastify error methods
     jest
       .spyOn(fastify.httpErrors, 'unauthorized')
       .mockImplementation((msg?: string) => {
-        throw new Error(msg || 'Unauthorized');
+        throw httpErrors.unauthorized(msg);
       });
     jest
       .spyOn(fastify.httpErrors, 'internalServerError')
       .mockImplementation((msg?: string) => {
-        throw new Error(msg || 'Internal server error');
+        throw httpErrors.internalServerError(msg);
       });
     jest
       .spyOn(fastify.httpErrors, 'notFound')
       .mockImplementation((msg?: string) => {
-        throw new Error(msg || 'Not found');
+        throw httpErrors.notFound(msg);
       });
   });
 
   describe('issueAuthorizationCode', () => {
     it('should return authorization code', async () => {
+      jest.spyOn(fastify.redis, 'set').mockResolvedValue('OK');
       const result = await service.issueAuthorizationCode(1);
       expect(result).toBeTruthy();
       expect(result).toHaveLength(32);
@@ -102,30 +113,37 @@ describe('TokenService', () => {
   });
 
   describe('issueTokenPair', () => {
-    const user: users = {
-      id: 1,
-      email: 'test@ifelfi.com',
-      uuid_key: uuidKey,
-    };
-    const profile: profile = {
-      id: 1,
-      user_id: user.id,
-      nickname: 'test',
-      image_url: null,
-      join_date: new Date(),
-      update_date: new Date(),
-    };
-
-    beforeAll(() => {
-      jest.spyOn(fastify.prisma.users, 'findUnique').mockResolvedValue(user);
-      jest.spyOn(fastify.prisma.profile, 'findUnique').mockResolvedValue(profile);
-    });
-
     it('should return token pair', async () => {
+      jest.spyOn(fastify.redis, 'get').mockResolvedValue(user.id.toString());
+      jest.spyOn(fastify.redis, 'del').mockResolvedValue(1);
+      jest.spyOn(fastify.prisma.users, 'findUnique').mockResolvedValue(user);
+      jest
+        .spyOn(fastify.prisma.profile, 'findUnique')
+        .mockResolvedValue(profile);
+
       const result = await service.issueTokenPair(code);
 
       expect(result.accessToken).toBeTruthy();
       expect(result.refreshToken).toBeTruthy();
+    });
+
+    it('should throw internal server error when getting code throw error', async () => {
+      jest.spyOn(fastify.redis, 'get').mockRejectedValue(new Error());
+      await expect(service.issueTokenPair(code)).rejects.toThrow(
+        'Get code error',
+      );
+    });
+
+    it('should throw not found error when code is not found', async () => {
+      jest.spyOn(fastify.redis, 'get').mockResolvedValue(null);
+      await expect(service.issueTokenPair(code)).rejects.toThrow(
+        'Code not found',
+      );
+    });
+
+    it('should throw internal server error when code is invalid', async () => {
+      jest.spyOn(fastify.redis, 'get').mockResolvedValue('invalid');
+      await expect(service.issueTokenPair(code)).rejects.toThrow('Code error');
     });
   });
 
@@ -163,43 +181,18 @@ describe('TokenService', () => {
 
   describe('validate', () => {
     it('should return true when access token is valid', async () => {
+      jest.spyOn(fastify.redis, 'get').mockResolvedValue(refreshToken);
       expect(await service.validate({ accessToken, refreshToken })).toBe(true);
     });
 
     it('should return true when access token is expired but refresh token is valid', async () => {
+      jest.spyOn(fastify.redis, 'get').mockResolvedValue(refreshToken);
       expect(
         await service.validate({
           accessToken: expiredAccessToken,
           refreshToken,
         }),
       ).toBe(true);
-    });
-
-    it('should return false when access token is invalid', async () => {
-      expect(
-        await service.validate({
-          accessToken: invalidAccessToken,
-          refreshToken,
-        }),
-      ).toBe(true);
-    });
-
-    it('should return true when refresh token is invalid but access token is valid', async () => {
-      expect(
-        await service.validate({
-          accessToken,
-          refreshToken: invalidRefreshToken,
-        }),
-      ).toBe(true);
-    });
-
-    it('should return false when access token is invalid payload', async () => {
-      expect(
-        await service.validate({
-          accessToken: wrongPayloadAccessToken,
-          refreshToken,
-        }),
-      ).toBe(false);
     });
 
     it('should return false when access token is expired and refresh token is expired', async () => {
@@ -210,6 +203,36 @@ describe('TokenService', () => {
         }),
       ).toBe(false);
     });
+  });
+
+  it('should return true when access token is invalid but refresh token is valid', async () => {
+    jest.spyOn(fastify.redis, 'get').mockResolvedValue(refreshToken);
+    expect(
+      await service.validate({
+        accessToken: invalidAccessToken,
+        refreshToken,
+      }),
+    ).toBe(true);
+  });
+
+  it('should return true when refresh token is invalid but access token is valid', async () => {
+    jest.spyOn(fastify.redis, 'get').mockResolvedValue(refreshToken);
+    expect(
+      await service.validate({
+        accessToken,
+        refreshToken: invalidRefreshToken,
+      }),
+    ).toBe(true);
+  });
+
+  it('should return false when access token is invalid payload', async () => {
+    jest.spyOn(fastify.redis, 'get').mockResolvedValue(refreshToken);
+    expect(
+      await service.validate({
+        accessToken: wrongPayloadAccessToken,
+        refreshToken,
+      }),
+    ).toBe(false);
   });
 
   describe('validateAndRefresh', () => {
