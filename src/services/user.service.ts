5fb00d22956704f058a1df213163f96c08b120d2
sauce-git <sauce@mandacode.com>
2024-04-27 16:10:18 +0900

    Update user.service.spec.ts to include provider information in getProfile method

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,3 +1,4 @@
+import { provider_type } from '@prisma/client';
 import { FastifyInstance } from 'fastify';
 
 export class UserService {
@@ -5,6 +6,68 @@ export class UserService {
 
   constructor(fastify: FastifyInstance) {
     this.#fastify = fastify;
+  }
+
+  /**
+   * Get user profile
+   * @param uuidKey User's UUID key
+   * @returns user profile
+   */
+  public async getProfile(uuidKey: string): Promise<{
+    email: string;
+    nickname: string;
+    imageUrl: string | null;
+    joinDate: Date;
+    updateDate: Date;
+    provider: provider_type;
+  }> {
+    const user = await this.#fastify.prisma.users
+      .findUnique({
+        where: { uuid_key: uuidKey },
+      })
+      .catch((error) => {
+        throw this.#fastify.httpErrors.internalServerError(
+          'Failed to find user',
+        );
+      });
+    if (!user) {
+      throw this.#fastify.httpErrors.notFound('User not found');
+    }
+
+    const profile = await this.#fastify.prisma.profile
+      .findUnique({
+        where: { user_id: user.id },
+      })
+      .catch((error) => {
+        throw this.#fastify.httpErrors.internalServerError(
+          'Failed to find user profile',
+        );
+      });
+    if (!profile) {
+      throw this.#fastify.httpErrors.notFound('User profile not found');
+    }
+
+    const provider = await this.#fastify.prisma.provider
+      .findUnique({
+        where: { user_id: user.id },
+      })
+      .catch((error) => {
+        throw this.#fastify.httpErrors.internalServerError(
+          'Failed to find user provider',
+        );
+      });
+    if (!provider) {
+      throw this.#fastify.httpErrors.notFound('User provider not found');
+    }
+
+    return {
+      email: user.email,
+      nickname: profile.nickname,
+      imageUrl: profile.image_url,
+      joinDate: profile.join_date,
+      updateDate: profile.update_date,
+      provider: provider.provider,
+    };
   }
 
   /**
