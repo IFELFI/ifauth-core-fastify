06ab0f0d91a6d47966062533328ef3f86a9b85e6
sauce-git <sauce@mandacode.com>
2024-04-29 21:32:49 +0900

    Refactor access token verification and error handling in user.e2e-spec.ts

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -82,56 +82,48 @@ describe('User', () => {
       expect(response.statusCode).toBe(200);
     });
 
-    it('should return 200 when get user profile with expired access token but refresh token is valid', async () => {
+    it('should return 401 when get user profile with expired access token', async () => {
       const response = await server.inject({
         method: 'GET',
         url: '/user/profile',
         headers: {
           Authorization: `Bearer ${data.accessToken.expired}`,
         },
-        cookies: {
-          refresh: signer.sign(data.refreshToken.normal),
-        },
       });
-      expect(response.statusCode).toBe(200);
+      expect(response.statusCode).toBe(401);
       expect(response.json()).toEqual({
-        message: 'User profile found',
-        data: expectedProfile,
+        error: 'Unauthorized',
+        message: 'Access token needs to be refreshed',
+        statusCode: 401,
       });
     });
 
-    it('should return 401 when get user profile with expired access token and refresh token', async () => {
+    it('should return 401 when get user profile with wrong payload access token', async () => {
       await redisClient.del(data.user.user.uuid_key);
-      const response = await server.inject({
-        method: 'GET',
-        url: '/user/profile',
-        headers: {
-          Authorization: `Bearer ${data.accessToken.expired}`,
-        },
-        cookies: {
-          refresh: signer.sign(data.refreshToken.expired),
-        },
-      });
-      expect(response.statusCode).toBe(401);
-    });
-
-    it('should return 401 when get user profile without access and refresh token', async () => {
-      const response = await server.inject({
-        method: 'GET',
-        url: '/user/profile',
-      });
-      expect(response.statusCode).toBe(401);
-    });
-
-    it('should return 401 when get user profile with an wrong payload access token', async () => {
       const response = await server.inject({
         method: 'GET',
         url: '/user/profile',
         headers: {
           Authorization: `Bearer ${data.accessToken.wrongPayload}`,
         },
-        cookies: {
-          refresh: signer.sign(data.refreshToken.normal),
+      });
+      expect(response.statusCode).toBe(401);
+    });
+
+    it('should return 401 when get user profile without access token', async () => {
+      const response = await server.inject({
+        method: 'GET',
+        url: '/user/profile',
+      });
+      expect(response.statusCode).toBe(401);
+    });
+
+    it('should return 401 when get user profile with an wrong secret access token', async () => {
+      const response = await server.inject({
+        method: 'GET',
+        url: '/user/profile',
+        headers: {
+          Authorization: `Bearer ${data.accessToken.wrongSecret}`,
         },
       });
       expect(response.statusCode).toBe(401);
@@ -146,25 +138,24 @@ describe('User', () => {
         headers: {
           Authorization: `Bearer ${data.accessToken.normal}`,
         },
-        cookies: {
-          refresh: signer.sign(data.refreshToken.normal),
-        },
       });
       expect(response.statusCode).toBe(200);
     });
 
-    it('should return 200 when logout with expired access token but refresh token is valid', async () => {
+    it('should return 401 when logout with expired access', async () => {
       const response = await server.inject({
         method: 'GET',
         url: '/user/logout',
         headers: {
           Authorization: `Bearer ${data.accessToken.expired}`,
         },
-        cookies: {
-          refresh: signer.sign(data.refreshToken.normal),
-        },
       });
-      expect(response.statusCode).toBe(200);
+      expect(response.statusCode).toBe(401);
+      expect(response.json()).toEqual({
+        error: 'Unauthorized',
+        message: 'Access token needs to be refreshed',
+        statusCode: 401,
+      });
     });
 
     it('should return 401 when logout with expired access token and refresh token', async () => {
@@ -173,21 +164,23 @@ describe('User', () => {
         method: 'GET',
         url: '/user/logout',
         headers: {
-          Authorization: `Bearer ${data.accessToken.expired}`,
-        },
-        cookies: {
-          refresh: signer.sign(data.refreshToken.normal),
+          Authorization: `Bearer ${data.accessToken.wrongSecret}`,
         },
       });
       expect(response.statusCode).toBe(401);
     });
 
-    it('should return 401 when logout without access and refresh token', async () => {
+    it('should return 401 when logout without access token', async () => {
       const response = await server.inject({
         method: 'GET',
         url: '/user/logout',
       });
       expect(response.statusCode).toBe(401);
+      expect(response.json()).toEqual({
+        error: 'Unauthorized',
+        message: 'Access token is required',
+        statusCode: 401,
+      });
     });
 
     it('should return 401 when logout with an wrong payload access token', async () => {
@@ -196,9 +189,6 @@ describe('User', () => {
         url: '/user/logout',
         headers: {
           Authorization: `Bearer ${data.accessToken.wrongPayload}`,
-        },
-        cookies: {
-          refresh: signer.sign(data.refreshToken.normal),
         },
       });
       expect(response.statusCode).toBe(401);
@@ -213,9 +203,6 @@ describe('User', () => {
         headers: {
           Authorization: `Bearer ${data.accessToken.normal}`,
         },
-        cookies: {
-          refresh: signer.sign(data.refreshToken.normal),
-        },
       });
       expect(response.statusCode).toBe(200);
     });
@@ -227,22 +214,32 @@ describe('User', () => {
         headers: {
           Authorization: `Bearer ${data.accessToken.expired}`,
         },
-        cookies: {
-          refresh: signer.sign(data.refreshToken.normal),
-        },
       });
-      expect(response.statusCode).toBe(200);
+      expect(response.statusCode).toBe(401);
+      expect(response.json()).toEqual({
+        error: 'Unauthorized',
+        message: 'Access token needs to be refreshed',
+        statusCode: 401,
+      });
     });
 
-    it('should return 401 when delete user with expired access token and refresh token', async () => {
+    it('should return 401 when delete user with wrong payload access token', async () => {
       const response = await server.inject({
         method: 'DELETE',
         url: '/user',
         headers: {
-          Authorization: `Bearer ${data.accessToken.expired}`,
+          Authorization: `Bearer ${data.accessToken.wrongPayload}`,
         },
-        cookies: {
-          refresh: signer.sign(data.refreshToken.expired),
+      });
+      expect(response.statusCode).toBe(401);
+    });
+
+    it('should return 401 when delete user with an wrong secret access token', async () => {
+      const response = await server.inject({
+        method: 'DELETE',
+        url: '/user',
+        headers: {
+          Authorization: `Bearer ${data.accessToken.wrongSecret}`,
         },
       });
       expect(response.statusCode).toBe(401);
