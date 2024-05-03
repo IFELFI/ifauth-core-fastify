64b2cbeaa2cb5fedbe4a613aa407e6c00716d3b1
sauce-Git <dadad1004@naver.com>
2024-05-03 09:37:37 +0900

    Update Dockerfile version to 2.2.2

--- src/routes/auth/local.route.ts
+++ src/routes/auth/local.route.ts
@@ -1,27 +1,32 @@
 import { FastifyTypebox } from '../../app';
-import { AuthReplyData, ReplyData } from '../../interfaces/reply.interface';
+import { AuthReplyData } from '../../interfaces/reply.interface';
 import { localLoginSchema, localSignupSchema } from '../../schema/auth.schema';
 
 export default async function (fastify: FastifyTypebox) {
   const basePath = '/auth/local';
 
+  /**
+   * Disable signup for now
+   * This method needs more validation and security
+   */
   fastify.post(
     `${basePath}/signup`,
     {
       schema: localSignupSchema,
     },
     async (request, reply) => {
-      const userId = await fastify.services.authLocalService.signup(
-        request.body,
-      );
-      const code =
-        await fastify.services.tokenService.issueAuthorizationCode(userId);
+      reply.forbidden('Signup is disabled for now.');
+      // const userId = await fastify.services.authLocalService.signup(
+      //   request.body,
+      // );
+      // const code =
+      //   await fastify.services.tokenService.issueAuthorizationCode(userId);
 
-      const replyData: AuthReplyData = {
-        message: 'User created',
-        code: code,
-      };
-      reply.code(201).send(replyData);
+      // const replyData: AuthReplyData = {
+      //   message: 'User created',
+      //   code: code,
+      // };
+      // reply.code(201).send(replyData);
     },
   );
 
