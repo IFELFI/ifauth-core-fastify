0d000804eae0d2809b541e2cfc490280ef63c8e5
sauce-git <sauce@mandacode.com>
2024-06-21 14:45:00 +0900

    chore: Update index.ts to include cookie in request logging

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,7 @@ async function run() {
             remoteAddress: req.ip,
             hostname: req.hostname,
             remotePort: req.connection.remotePort,
+            cookie: JSON.stringify(req.cookies),
           };
         },
       },
