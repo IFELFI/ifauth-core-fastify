46268b286bba53bdad1ea40498f134ba759192c0
sauce-git <sauce@mandacode.com>
2024-06-21 13:46:30 +0900

    chore: Update Dockerfile version to 2.2.5-rc5

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:21-alpine as build
 
 LABEL title="ifauth-core"
-LABEL version="2.2.5-rc4"
+LABEL version="2.2.5-rc5"
 LABEL maintainer="ifelfi"
 
 WORKDIR /app
--- src/app.ts
+++ src/app.ts
@@ -99,7 +99,7 @@ async function build(opts: {}, data: any = process.env) {
   });
   await app.register(helmet, { global: true });
   await app.register(cors, {
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://ifelfi.com'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
