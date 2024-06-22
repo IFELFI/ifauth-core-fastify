d8e759ec5d85894845c90c2ce6141879088908d3
sauce <dadad1004@naver.com>
2024-06-22 16:42:32 +0900

    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ async function run() {
             remoteAddress: req.ip,
             hostname: req.hostname,
             remotePort: req.connection.remotePort,
-            cookie: JSON.stringify(req.cookies),
+            cookie: JSON.stringify(req.headers['set-cookie']),
           };
         },
       },
